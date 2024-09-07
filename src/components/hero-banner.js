fdc395c1683565fde465cd3f213accae2fe62069
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-09-07 15:03:20 -0400

    Update HeroBanner component

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -31,56 +31,29 @@ export const HeroBanner = () => {
                   zIndex: -1,
                 }}
               >
-                Xarray
+                Pangeo
               </Text>
               <br />
               <Text as={'span'} color={'red.400'}>
-                N-D labeled arrays and datasets in Python
+                A community for open, reproducible, scalable geoscience.
               </Text>
             </Heading>
             <Text fontSize={'lg'}>
-              <strong>Xarray</strong> is an open source project and Python
-              package that introduces labels in the form of dimensions,
-              coordinates, and attributes on top of raw NumPy-like arrays, which
-              allows for more intuitive, more concise, and less error-prone user
-              experience.
-              <br />
-              <br />
-              Xarray includes a large and growing library of domain-agnostic
-              functions for advanced analytics and visualization with these data
-              structures.
+              <strong>Pangeo</strong> is first and foremost a community
+              promoting open, reproducible, and scalable science. This community
+              provides documentation, develops and maintains software, and
+              deploys computing infrastructure to make scientific research and
+              programming easier.
             </Text>
-          </Stack>
-          <Stack flex={1} spacing={{ base: 10, md: 20 }}>
-            <Image
-              src={'/xarray-datastructure.png'}
-              alt='xarray data structure'
-              objectFit='contain'
-            ></Image>
-
-            <Stack
-              spacing={{ base: 4, sm: 6 }}
-              justify={'center'}
-              direction={{ base: 'column', sm: 'row' }}
+            <Button
+              useExternalIcon
+              as={Link}
+              variant={'outline'}
+              colorScheme={'blue'}
+              href='https://discourse.pangeo.io/'
             >
-              <Button
-                useExternalIcon
-                as={Link}
-                variant={'outline'}
-                colorScheme={'blue'}
-                href='https://docs.xarray.dev/en/stable/getting-started-guide/quick-overview.html'
-              >
-                Get Started
-              </Button>
-              <Button
-                useExternalIcon
-                as={Link}
-                variant={'outline'}
-                href='https://docs.xarray.dev/en/stable/getting-started-guide/why-xarray.html'
-              >
-                Why Xarray?
-              </Button>
-            </Stack>
+              Join the Discourse
+            </Button>
           </Stack>
         </Stack>
       </Container>
