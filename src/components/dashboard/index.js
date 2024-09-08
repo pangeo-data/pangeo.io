7e21087434fdaff86a6eb8b5e55b3cf21027a6ec
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-09-08 13:58:08 -0400

    Remove dashboard

--- src/components/dashboard/index.js
+++
@@ -1,6 +0,0 @@
-export { DatasetteStatsCard } from '@/components/dashboard/datasette-stats-card'
-export { IssueTracker } from '@/components/dashboard/issue-tracker'
-export { ProjectMetrics } from '@/components/dashboard/project-metrics'
-export { StatisticsCard } from '@/components/dashboard/statistics-card'
-export { TimelinePlotContainer } from '@/components/dashboard/timeline-plot-container'
-export { TimeseriesAggStatsCard } from '@/components/dashboard/timeseries-agg-stats-card'
